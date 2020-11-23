f61440bbcd89cbf4a51534add834c9197031d0bc
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2020-11-23 14:55:51 +0700

    Changelog 3.1.1 : - Update page.js route

--- routes/page.js
+++ routes/page.js
@@ -5,21 +5,28 @@ const helper = require('../lib/helper.js')
 const pjson = require('../package.json')
 
 async function pageRoute (server, options) {
+  /**
+   * Default variable for template engine
+   */
+  const templateData = {
+    baseUrl: config.baseUrl,
+    baseAssetsUrl: config.baseAssetsUrl,
+    year: helper.copyrightYear(config.startYearCopyright),
+    siteName: config.siteName,
+    siteDescription: config.siteDescription,
+    authorName: config.authorName,
+    authorEmail: config.authorEmail,
+    authorWebsite: config.authorWebsite,
+    webmaster: config.webmaster,
+    tracker: config.tracker,
+    version: pjson.version
+  }
+
   server.get('/', { onRequest: server.useHtmlCache }, async (request, reply) => {
-    const html = await server.view('index', {
-      baseUrl: config.baseUrl,
-      baseAssetsUrl: config.baseAssetsUrl,
-      year: helper.copyrightYear(config.startYearCopyright),
-      siteName: config.siteName,
-      siteTitle: config.siteTitle,
-      siteDescription: config.siteDescription,
-      authorName: config.authorName,
-      authorEmail: config.authorEmail,
-      authorWebsite: config.authorWebsite,
-      webmaster: config.webmaster,
-      tracker: config.tracker,
-      version: pjson.version
-    })
+    // Shallow Clone
+    const newTemplateData = { ...templateData }
+    newTemplateData.siteTitle = config.siteTitle
+    const html = await server.view('index', newTemplateData)
     await reply.code(200).header('Content-Type', 'text/html; charset=utf-8').send(html)
   })
 }
